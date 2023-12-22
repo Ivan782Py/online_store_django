a7cbdd57412bba3d3cd2fb70364e036a297ce3a3
Ivan782Py <ipyatkov86@gmail.com>
2023-12-22 11:50:13 +0700

    added fixtures

--- prospect/settings.py
+++ prospect/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("PROJECT_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
